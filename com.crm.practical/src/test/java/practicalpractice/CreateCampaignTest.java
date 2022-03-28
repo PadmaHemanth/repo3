1a89000974ffa7d5ffaa7b6e4d57364f902d2469
padma <padma@DESKTOP-FM8OD3M>
2022-03-28 14:21:10 +0530

    monday push

---
+++ com.crm.practical/src/test/java/practicalpractice/CreateCampaignTest.java
@@ -0,0 +1,16 @@
+package practicalpractice;
+
+import org.testng.annotations.Test;
+
+public class CreateCampaignTest {
+	
+	@Test
+	public void createCampaignNameTest() {
+		
+		System.out.println("campaign name is created");
+	}
+	@Test
+	public void createCampaignIndustry() {
+	System.out.println("industry created");
+}
+}
